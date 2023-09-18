586abb55a82810519c37764708c76a62f5c2a54d
kingsparkdev <kingsparkdev@gmail.com>
2023-09-19 08:15:27 +0900

    synk correction

--- public/js/learn.js
+++ public/js/learn.js
@@ -1,8 +1,8 @@
 $('.link-dark').click(function () {
     const category = $(this).data('category');
-    $('.learn-subtitle').text(category.replaceAll('-',' '));
+    // $('.learn-subtitle').text(category.replaceAll('-',' '));
     const title = $(this).data('title');
-    $('.learn-title').text(title.replaceAll('-',' '));  
+    // $('.learn-title').text(title.replaceAll('-',' '));  
     console.log(`/${category.replaceAll('-','_')}/${title.replaceAll('-','_')}.html`);
     $('.content-container').load(`/learnContent/${category.replaceAll('-','_')}/${title.replaceAll('-','_')}.html`);
 })
