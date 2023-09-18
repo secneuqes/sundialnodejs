47ce4fa8a0485d519286aa2149aa8c36ed2bcc6b
kingsparkdev <kingsparkdev@gmail.com>
2023-09-18 23:10:00 +0900

    changed images

--- public/js/learn.js
+++ public/js/learn.js
@@ -4,5 +4,11 @@ $('.link-dark').click(function () {
     const title = $(this).data('title');
     $('.learn-title').text(title.replaceAll('-',' '));  
     console.log(`/${category.replaceAll('-','_')}/${title.replaceAll('-','_')}.html`);
-    $('.content-container').load(`/learnContent/${category.replaceAll('-','_')}/${title.replaceAll('-','_')}.html`)
+    $('.content-container').load(`/learnContent/${category.replaceAll('-','_')}/${title.replaceAll('-','_')}.html`);
+})
+
+$(function() {
+    $('.learn-subtitle').text("앙부일구");
+    $('.learn-title').text("개요");  
+    $('.content-container').load(`/learnContent/앙부일구/개요.html`)
 })
